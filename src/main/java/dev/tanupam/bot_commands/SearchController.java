492475d8ef8e240984ce27e5c1c0b52d0b5667bc
coder_17 <tanupamsaha26@gmail.com>
2023-05-24 19:20:02 +0530

    Final Commit.

--- src/main/java/dev/tanupam/bot_commands/SearchController.java
+++ src/main/java/dev/tanupam/bot_commands/SearchController.java
@@ -4,12 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Message;
-import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.EditMessageText;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import dev.tanupam.model.ChatGPTReq;
 import dev.tanupam.model.ChatGPTRes;
+import org.apache.logging.log4j.util.StringBuilderFormattable;
 
 import java.io.IOException;
 import java.net.URI;
@@ -22,7 +22,7 @@ public class SearchController {
 
         SendResponse sendResponse = bot.execute(new SendMessage(chat.id(), "Fetching results, please wait.").replyToMessageId(message.messageId()));
         ObjectMapper objectMapper = new ObjectMapper();
-        ChatGPTReq chatGPTReq = new ChatGPTReq("text-davinci-003", query, 0, 3996);
+        ChatGPTReq chatGPTReq = new ChatGPTReq("text-davinci-003", query, 1, 3996);
         String input = objectMapper.writeValueAsString(chatGPTReq);
             HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api.openai.com/v1/completions")).header("Content-Type", "application/json").header("Authorization", ("Bearer %s").formatted(System.getenv("OPENAI_TOKEN"))).POST(HttpRequest.BodyPublishers.ofString(input)).build();
             HttpClient client = HttpClient.newHttpClient();
@@ -31,7 +31,7 @@ public class SearchController {
             if (response.statusCode() == 200) {
                 ChatGPTRes chatGPTRes = objectMapper.readValue(response.body(), ChatGPTRes.class);
                 String answer = chatGPTRes.choices()[chatGPTRes.choices().length - 1].text();
-                StringBuffer questionResponse = new StringBuffer();
+                StringBuilder questionResponse = new StringBuilder();
                 if(answer == null) {
                     EditMessageText editMessageText = new EditMessageText(chat.id(), sendResponse.message().messageId(), "Sorry, could not understand the question. Please try rephrasing your question.");
                     bot.execute(editMessageText);
